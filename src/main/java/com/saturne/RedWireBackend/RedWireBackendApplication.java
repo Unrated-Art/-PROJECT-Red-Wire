e06a340a427d74fc22a9bd0c1cb81f702928f558
Eternal-Grace <91600327+Eternal-Grace@users.noreply.github.com>
2022-08-03 17:19:46 +0200

    replace content from dev_nourhene

---
+++ src/main/java/com/saturne/RedWireBackend/RedWireBackendApplication.java
@@ -0,0 +1,13 @@
+package com.saturne.RedWireBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedWireBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedWireBackendApplication.class, args);
+	}
+
+}
