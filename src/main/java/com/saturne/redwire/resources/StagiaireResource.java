486cc3533ac91cc986b70c589ca8f5678100c9a7
Eternal Grace <91600327+Eternal-Grace@users.noreply.github.com>
2022-08-29 15:44:54 +0200

    feature get+edit stagiaire

--- src/main/java/com/saturne/redwire/resources/StagiaireResource.java
+++ src/main/java/com/saturne/redwire/resources/StagiaireResource.java
@@ -6,14 +6,21 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.saturne.redwire.entities.Formation;
 import com.saturne.redwire.entities.Stagiaire;
 import com.saturne.redwire.services.StagiaireService;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,32 +31,41 @@ public class StagiaireResource {
 
   private final StagiaireService sf;
 
+  @Autowired
   public StagiaireResource(StagiaireService sf) {
     this.sf = sf;
   }
 
-  @GetMapping(name = "get.stagiaire")
+  @GetMapping(name = "get.stagiaire", path="", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<Stagiaire> getStagiaire(@RequestHeader Map<String, String> headers) {
-    String token = headers.get("authorization").toString();
-    Map<String, Claim> response = this.getDataFromToken(token);
-    String idsCh = response.get("idStagiaire").toString();
-    idsCh = idsCh.substring(1, idsCh.length() - 1);
-    Stagiaire stagiaire = sf.findStagiaireById(Long.parseLong(idsCh));
-    System.out.println("Headers= " + headers);
-    return new ResponseEntity<>(stagiaire, HttpStatus.ACCEPTED);
+	  String token = headers.get("authorization").toString();
+	  Stagiaire stagiaire = this.getStagiaireFromToken(token);
+	  return new ResponseEntity<>(stagiaire, HttpStatus.ACCEPTED);
   }
 
-  @PutMapping(name = "update.stagiaire")
-  public ResponseEntity<Stagiaire> updateStagiaire(@RequestHeader Map<String, String> headers) {
-    String token = headers.get("authorization").toString();
-    Map<String, Claim> response = this.getDataFromToken(token);
-    String idsCh = response.get("idStagiaire").toString();
-    idsCh = idsCh.substring(1, idsCh.length() - 1);
-    Stagiaire stagiaire = sf.findStagiaireById(Long.parseLong(idsCh));
-    return new ResponseEntity<>(stagiaire, HttpStatus.OK);
+  @PutMapping(name = "update.stagiaire", path="", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Stagiaire> updateStagiaire(
+		  @RequestHeader Map<String, String> headers,
+		  @RequestBody Map<String, String> body
+	  ) {
+	  String token = headers.get("authorization").toString();
+	  Stagiaire stagiaire = this.getStagiaireFromToken(token);
+
+	  stagiaire.setNom(body.get("nom").toString());
+	  stagiaire.setPrenom(body.get("prenom").toString());
+	  stagiaire.setAdresse(body.get("adresse").toString());
+	  stagiaire.setEmail(body.get("email").toString());
+	  stagiaire.setNumTelephone(body.get("numTelephone").toString());
+	  stagiaire.setMpass(body.get("mpass").toString());
+	  stagiaire.setEntreprise(Boolean.parseBoolean(body.get("entreprise").toString()));
+	  stagiaire.setCoordonneesEntre(body.get("coordonneesEntre").toString());
+
+	  stagiaire = sf.updateStagiaire(stagiaire);
+	  System.out.println("stagiaire " + stagiaire);
+	  return new ResponseEntity<Stagiaire>(stagiaire, HttpStatus.OK);
   }
 
-  private Map<String, Claim> getDataFromToken(String token) {
+  private Stagiaire getStagiaireFromToken(String token) {
     Map<String, Claim> data = new HashMap<>();
     try {
       Algorithm algorithm = Algorithm.HMAC256("redwire"); //use more secure key
@@ -60,6 +76,8 @@ public class StagiaireResource {
     } catch (JWTVerificationException exception) {
       System.out.println(exception.getMessage());
     }
-    return data;
+    String idsCh = data.get("idStagiaire").toString();
+    idsCh = idsCh.substring(1, idsCh.length() - 1);
+    return sf.findStagiaireById(Long.parseLong(idsCh));
   }
 }
